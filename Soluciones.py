8af67428e5ded19b4f95ecd56677f6901b4287f8
LoretoLopez <145747001+LoretoLopez@users.noreply.github.com>
2023-09-29 09:07:12 +0200

    Añado soluciones

---
+++ Soluciones.py
@@ -0,0 +1,5 @@
+1. La serpiente venenosa con un patrón de colores brillantes es Ekans.
+2. El Pokémon con una concha dura y cañones de agua en su espalda es Squirtle.
+3. El pájaro grande de tipo volador es Pidgeot.
+4. El dragón que lanza llamas ardientes de su boca es Charizard.
+5. El pequeño roedor eléctrico con mejillas rojas y amarillas es Pikach
